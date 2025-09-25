de9bfa6112589a453fafa209f2c0b93132272ee1
unknown <nguyen_van_phu@allgrow-labo.jp>
2025-09-25 08:30:06 +0700

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// ===== globals =====
+const isMobile = window.matchMedia("(max-width: 1024px)");
+const eventsTrigger = ["pageshow", "scroll"];
+
+// ===== init =====
+const init = () => {
+  // # app height
+  appHeight();
+  // # lazy load
+  const ll = new LazyLoad({
+    threshold: 100,
+    elements_selector: ".lazy",
+  });
+};
+
+// ===== lenis =====
+window.lenis = new Lenis({
+  duration: 1.0,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(1 - t, 2.5)),
+  smooth: true,
+  mouseMultiplier: 1.0,
+  smoothTouch: true,
+  touchMultiplier: 1.5,
+  infinite: false,
+  direction: "vertical",
+  gestureDirection: "vertical",
+});
+const raf = (t) => {
+  window.lenis.raf(t);
+  requestAnimationFrame(raf);
+};
+requestAnimationFrame(raf);
+
+// ===== app height =====
+const appHeight = () => {
+  const doc = document.documentElement;
+  const menuH = Math.max(doc.clientHeight, window.innerHeight || 0);
+
+  if (isMobile.matches) {
+    doc.style.setProperty("--app-height", `${doc.clientHeight}px`);
+    doc.style.setProperty("--menu-height", `${menuH}px`);
+  } else {
+    doc.style.removeProperty("--app-height");
+    doc.style.removeProperty("--menu-height");
+  }
+};
+window.addEventListener("resize", appHeight);
+
+// ### ===== DOMCONTENTLOADED ===== ###
+window.addEventListener("DOMContentLoaded", init);
