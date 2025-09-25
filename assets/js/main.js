426ef118525ef6dcb09bbb0f9170ef68b006644b
unknown <nguyen_van_phu@allgrow-labo.jp>
2025-09-25 15:06:57 +0700

    coding section fv done

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ const init = () => {
 const lenis = new Lenis({
   duration: 1.0,
   easing: (t) => Math.min(1, 1.001 - Math.pow(1 - t, 2.5)),
+  // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   smooth: true,
   mouseMultiplier: 1.0,
   smoothTouch: true,
@@ -54,6 +55,45 @@ const appHeight = () => {
   }
 };
 window.addEventListener("resize", appHeight);
+
+// ===== scroll trigger =====
+gsap.registerPlugin(ScrollTrigger);
+
+// # change bg
+const defaultColor = getComputedStyle(document.body).backgroundColor;
+document.querySelectorAll("[data-bg]").forEach((panel) => {
+  let color = panel.dataset.bg;
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    markers: false,
+    onEnter: () =>
+      gsap.to("body", {
+        duration: 0.3,
+        ease: "power1.inOut",
+        backgroundColor: color,
+      }),
+    onLeaveBack: () =>
+      gsap.to("body", {
+        duration: 0.3,
+        ease: "power1.inOut",
+        backgroundColor: defaultColor,
+      }),
+  });
+});
+
+// # hide tree
+gsap.to("[data-fv-tree]", {
+  autoAlpha: 0,
+  duration: 0.3,
+  ease: "sine.inOut",
+  scrollTrigger: {
+    trigger: "[data-diary]",
+    start: "top bottom",
+    markers: false,
+    toggleActions: "play reverse play reverse",
+  },
+});
 
 // ### ===== DOMCONTENTLOADED ===== ###
 window.addEventListener("DOMContentLoaded", init);
